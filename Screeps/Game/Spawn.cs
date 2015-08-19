3dbc44ae8bf0772e71d7830a637f38ac5e893120
zedr0n <zedr0nre@gmail.com>
2015-08-19 23:34:57 +0100

    createCreep implementation

---
+++ Screeps/Game/Spawn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge;
+
+namespace Screeps.Bridge
+{
+    [Ignore]
+    public class Spawn
+    {
+        private string name = "";
+        private string basePrefix = "";
+
+        public void createCreep(Creep.Spec spec, string name)
+        {
+            _createCreep(spec.ToArray(),name);
+        }
+
+
+        [Template("{this.prefix : raw}[{this.name}].createCreep({props : raw}, {name})")]
+        private void _createCreep(Creep.Property[] props, string name)
+        {
+
+        }
+
+        public Spawn(string basePrefix,string name)
+        {
+            this.name = name;
+            this.basePrefix = basePrefix;
+        }
+
+    }
+}
